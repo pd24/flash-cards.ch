8d7a47402ec670f6be6ed49c19fef7fd92a8b044
pd24 <pekent.durmaz@gmail.com>
2020-05-15 11:52:48 +0200

    small test of cd

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -38,7 +38,7 @@ class StartScreen extends React.Component{
             <Row>
               <Col>
                 <Jumbotron bg="light">
-                  <h1>Welcome!</h1>
+                  <h1>Welcome !</h1>
                     <p>
                       On this page you will find a quiz on the subject of Introduction to Programming to check your learning progress and strengthen your knowledge.
                     </p>
